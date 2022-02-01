b8d88013a1ed212a8065f16d38cb1c40023c4cf4
Kseniya Smirnova <fanni@bk.ru>
2022-02-01 11:36:43 +0300

    3 todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoList } from './TodoList';
 
 const App = () => {
   return (
   <div>
-  <TodoList />  
+  <TodoList title= 'What to learn' />  
+  <TodoList title = 'Songs' />  
+  <TodoList title = 'Books'/>  
 </div>
   );
 }
-const TodoList = ()=> {
-  return (
-<div>
-    <h3>What to learn</h3>
-<div>
-<input /><button>+</button>
-</div>
-  <ul>
-    <li> <input type ='checkbox' checked = {true } /> <span> HTML&CSS </span> </li>
-    <li> <input type ='checkbox' checked = {true } /> <span> JS </span> </li>
-    <li> <input type ='checkbox' checked = {false } /> <span>React </span> </li>
-  </ul>
-  <div>
-    <button>All</button>
-    <button>Active</button>
-    <button>Completed</button>
-  </div>
-</div>
-  );
-}
-
-
-
-
-
 export default App;
